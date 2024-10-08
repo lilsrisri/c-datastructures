8d5c35254df8efe0e51e1e07510ef1dca3e0b3e1
U-BSD\463334 <463334@SHS-C6W2NW3.beaverton.k12.or.us>
2024-10-08 09:17:21 -0700

    hi

---
+++ test3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char str[5];
+  char str2[5];
+  cin >> str;
+  cin >> str2;
+
+  cout <<  str << endl;
+  cout << str2 << endl;
+  return 0;
+}
